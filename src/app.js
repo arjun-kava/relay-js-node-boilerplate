05879b7ace0de3e5225b0ab4a5972d494e415c66
arjun <arjunkava91@gmail.com>
2020-12-15 17:24:46 +0530

    apollo server setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const http = require("http");
+const { ApolloServer } = require("apollo-server-express");
+const { schema } = require("./schema/schema");
 
 const app = express();
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app });
+
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
 
 exports.app = app;
+exports.server = server;
+exports.httpServer = httpServer;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,11 @@
-const { graphqlHTTP } = require("express-graphql");
-const { schema } = require("./schema/schema");
-const { app } = require("./app");
+const { httpServer, server } = require("./app");
 require("events").EventEmitter.defaultMaxListeners = 20;
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  })
-);
-
-app.listen(4000);
+httpServer.listen({ port: process.env.SERVER_PORT }, () => {
+  console.log(
+    `🚀 Server ready at http://localhost:${process.env.SERVER_PORT}${server.graphqlPath}`
+  );
+  console.log(
+    `🚀 Subscriptions ready at ws://localhost:${process.env.SERVER_PORT}${server.subscriptionsPath}`
+  );
+});
